ea4f0f6d0a98c54245051cc562ec2dc710a55049
Kennethy <kennethymov@gmail.com>
2022-11-08 16:38:04 -0300

    Criação da migration tabela users

---
+++ src/database/migrations/20221108193423-create-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: { 
+        type: Sequelize.INTEGER,
+        allowNull: false, 
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false, 
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      bithDate: {
+        type: Sequelize.STRING,
+        allowNull: false, 
+      }
+    },
+    {
+      timestamps: false,
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
